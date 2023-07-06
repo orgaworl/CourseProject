6298e214a0b9179606c00e05d63d391e3e5bec61
orgaworl <orgaworl@outlook.com>
2023-07-06 23:21:34 +0800

    refactor: modify SM3 code in Project4

--- Project 4 optimize SM3 implementation (software)/SM3.cpp
+++ Project 4 optimize SM3 implementation (software)/SM3.cpp
@@ -5,12 +5,16 @@
 #include<ctime>
 using namespace std;
 
+
+
 clock_t sT;
 clock_t eT;
 double T;
-//#define maxLength 67108864
-//uint8 input[maxLength];
-//uint8 buf[maxLength];
+
+
+//#define maxLen 67108864
+//uint8 input[maxLen];
+//uint8 buf[maxLen];
 void testSM3HashFunction()
 {
 	//test2:
@@ -23,7 +27,9 @@ void testSM3HashFunction()
 			SM3Hash(input, strlen((const char*)input), buf);
 		}
 		eT = clock();
-	
+		
+
+
 	printf("明文: \"%s\"\n明文: ",input);
 	for (int j = 0; j < strlen((const char*)input); j++)
 	{
@@ -52,13 +58,13 @@ void testSM3HashFunction()
 	
 	//test4:
 	//int loopTimes = 100;
-	//for (int i = 0; i < maxLength; i++)
+	//for (int i = 0; i < maxLen; i++)
 	//{
 	//	input[i] = rand();
 	//}
 	//SM3Hash(input, 1048576, buf);
 	//SM3Hash(input, 1048576, buf);
-	//for (int l = 1048576; l <= maxLength; l += 1048576)
+	//for (int l = 1048576; l <= maxLen; l += 1048576)
 	//{
 	//	sT = clock();
 	//	for (int i = 0; i < loopTimes; i++)
@@ -73,8 +79,8 @@ void testSM3HashFunction()
 
 	//test5 :
 	//uint8 input[] = "202100460116";
-	//uint64 hashValue;
-	//hashValue=reducedSM3(input, strlen((const char*)input));
+	//uint64 hashVal;
+	//hashVal=reducedSM3(input, strlen((const char*)input));
 
 
 }
@@ -82,31 +88,31 @@ void testSM3HashFunction()
 
 
 uint32 IV[8] = { 0x7380166f,0x4914b2b9 ,0x172442d7 ,0xda8a0600 ,0xa96f30bc ,0x163138aa ,0xe38dee4d ,0xb0fb0e4e };
-int SM3Hash(uint8*plainText,long plainLength, uint8  hashValue[32])//[32]
+//mesLen 以字节为单位
+int SM3Hash(uint8*mesText,long long mesLen, uint8  hashVal[32])//[32]
 {
-	int blockSize = plainLength /64 + 1;
-	if (plainLength * 8 % 512 >= 448) {blockSize++;}
+	int blockSize = mesLen /64 + 1;
+	if (mesLen * 8 % 512 >= 448) {blockSize++;}
 
 	uint32* wordList;
-	uint32* linkVariable=IV;
-	uint32** messageBlock = padding(plainText,plainLength,blockSize);
+	uint32* linkVar=IV;
+	uint32** messageBlock = padding(mesText,mesLen,blockSize);
 	uint32* temp;
 	for (int i=0;i<blockSize;i++)
 	{
-		temp = linkVariable;
-		wordList = messageExtend(messageBlock[i]);
-		linkVariable = compress(wordList,linkVariable);
+		temp = linkVar;
+		linkVar = compress(messageBlock[i], linkVar);
 		if (i != 0) { delete temp; }
 	}
 	for (int i = 0; i < 8; i++)
-	{
+	{   //大小端转换
 		int i4 = i * 4;
-		hashValue[i4    ] = linkVariable[i] >> 24;
-		hashValue[i4 + 1] = linkVariable[i] >> 16;
-		hashValue[i4 + 2] = linkVariable[i] >> 8;
-		hashValue[i4 + 3] = linkVariable[i] ;
+		hashVal[i4    ] = linkVar[i] >> 24;
+		hashVal[i4 + 1] = linkVar[i] >> 16;
+		hashVal[i4 + 2] = linkVar[i] >> 8;
+		hashVal[i4 + 3] = linkVar[i] ;
 	}
-	delete linkVariable;
+	delete linkVar;
 	for (int i=0;i<blockSize;i++)
 	{
 		delete messageBlock[i];
@@ -125,7 +131,9 @@ uint32 loopLeftShift(uint32 x, int dis)
 	return ((x << dis) + (x >> (32 - dis)));
 }
 
-uint32** padding(uint8*plainText, long plainLength,int blockSize)
+
+//mesLen 以字节为单位
+uint32** padding(uint8*mesText, long long mesLen, long long blockSize)
 {
 	//一维数组转 二维矩阵
 	//lenght*8bit->blockSize * 16 *32bit
@@ -146,11 +154,11 @@ uint32** padding(uint8*plainText, long plainLength,int blockSize)
 		pos = 64 * i;
 		for (int j = 0; j < 16; j++)
 		{
-			temp1 = (plainText[pos] << 24);
-			temp2 = (plainText[pos + 1] << 16);
+			temp1 = (mesText[pos] << 24);
+			temp2 = (mesText[pos + 1] << 16);
 			temp1 += temp2;
-			temp3 = (plainText[pos + 2] << 8);
-			temp4 = (plainText[pos + 3]);
+			temp3 = (mesText[pos + 2] << 8);
+			temp4 = (mesText[pos + 3]);
 			temp3 += temp4;
 			mesBlock[i][j] = temp1 + temp3;
 			pos += 4;
@@ -160,18 +168,18 @@ uint32** padding(uint8*plainText, long plainLength,int blockSize)
 
 	//2. padding 最后一个块
 	mesBlock[blockSize - 1] = new uint32[16]{ 0 };
-	int block8Num = plainLength % 64; //剩余8bit块数量
+	int block8Num = mesLen % 64; //剩余8bit块数量
 	int block32Num = block8Num / 4;//剩余32bit块数量
 
 	//2.1 向最后512bit中填充剩下的32bit块
 	pos = (64 * (blockSize - 1));
 	for (int j=0;j<block32Num;j++)
 	{
-		temp1 = (plainText[pos] << 24);
-		temp2 = (plainText[pos + 1] << 16);
+		temp1 = (mesText[pos] << 24);
+		temp2 = (mesText[pos + 1] << 16);
 		temp1 += temp2;
-		temp3 = (plainText[pos + 2] << 8);
-		temp4 = (plainText[pos + 3]);
+		temp3 = (mesText[pos + 2] << 8);
+		temp4 = (mesText[pos + 3]);
 		temp3 += temp4;
 		mesBlock[blockSize - 1][j] = temp1 + temp3;
 		pos += 4;
@@ -181,7 +189,7 @@ uint32** padding(uint8*plainText, long plainLength,int blockSize)
 	block8Num = block8Num - 4 * block32Num;
 	for (int k=0;k<block8Num;k++)
 	{
-		mesBlock[blockSize - 1][block32Num] += (plainText[pos + k] << (8 * (3 - k)));
+		mesBlock[blockSize - 1][block32Num] += (mesText[pos + k] << (8 * (3 - k)));
 	}
 	
 	//2.3 写1bit 0b1
@@ -198,10 +206,10 @@ uint32** padding(uint8*plainText, long plainLength,int blockSize)
 
 
 	//2.4 写64bit长度值(注意大端小端)
-	uint32 temp32bit = ((long)(8 * plainLength) >> 31);
+	uint32 temp32bit = ((long)(8 * mesLen) >> 31);
 	temp32bit >>= 1;
 	mesBlock[blockSize - 1][14] = temp32bit;
-	mesBlock[blockSize - 1][15] = (8 *plainLength);
+	mesBlock[blockSize - 1][15] = (8 *mesLen);
 	return mesBlock;
 
 }
@@ -237,20 +245,33 @@ uint32* messageExtend(uint32 mes[16])
 	return wordList;
 }
 
-uint32* compress(uint32 *wordList,uint32 *linkVariable)
+uint32* compress(uint32 mes[16],uint32 linkVar[8])
 {
 	//将68+64个消息字和
 	//上个压缩函数的输出值8*32bit
 	//作为输入,输出压缩值256bit=8*32bit
 
 	//共进行64次,每次传入一对word和linkVarliable
+	//printf("\nlink: ");
+	//for (int i = 0; i < 8; i++)
+	//{
+	//	printf("%08x", linkVar[i]);
+	//}
+	//printf("\n");
 
 
+	//printf("\nword: ");
+	//for (int i=0;i<16;i++)
+	//{
+	//	printf("%08x",mes[i]);
+	//}
+	//printf("\n");
+
+	uint32* wordList = messageExtend(mes);
 	uint32* poiSrc = new uint32[8];
 	uint32* poiDes = new uint32[8];
 	uint32* poiTemp;
 	uint32* result=new uint32[8];
-
 
 	uint32 SS1=0;
 	uint32 SS2=0;
@@ -259,14 +280,10 @@ uint32* compress(uint32 *wordList,uint32 *linkVariable)
 	uint32 A=0, B=0, C=0;
 	uint32 E=0, F=0, G=0;
 
-
 	for (int i = 0; i < 8; i++)
 	{
-		poiDes[i] = linkVariable[i];
+		poiDes[i] = linkVar[i];
 	}
-
-
-	//第33轮出错
 	for (int i=0;i<64;i++)
 	{
 		//将指向本轮输出的指针指向上轮输出
@@ -274,29 +291,20 @@ uint32* compress(uint32 *wordList,uint32 *linkVariable)
 		poiDes = poiSrc;
 		poiSrc = poiTemp;
 
-
 		//计算本轮输出并保存到poiDes指向的堆数组
 		A = poiSrc[0];
 		E = poiSrc[4];
-
-
-
-
 		uint32 temp1 = ((A << 12) + (A >> 20));
 		uint32 temp2 ;
-		if (i<16)
-		{
+		if (i<16){
 			temp2 = loopLeftShift(0x79cc4519, i);
 		}
-		else
-		{
+		else{
 			temp2= loopLeftShift(0x7a879d8a, i);
 		}
-
 		SS1 = temp1+ E + temp2;
 		SS1 = ((SS1 << 7) + (SS1 >> 25));
 		SS2 = SS1 ^ temp1;
-
 		B = poiSrc[1];
 		C = poiSrc[2];
 		if (i<16){
@@ -317,24 +325,20 @@ uint32* compress(uint32 *wordList,uint32 *linkVariable)
 		}
 		TT2 =temp2 + poiSrc[7] + SS1 + wordList[i];;
 
-
 		poiDes[0] = TT1;
 		poiDes[1] = A;
 		poiDes[2] = ((B << 9) + (B >> 23));
 		poiDes[3] = C;
-
 		poiDes[4] = TT2 ^ ((TT2 << 9) + (TT2 >> 23)) ^ ((TT2 << 17) + (TT2 >> 15));//P0
 		poiDes[5] = E;
 		poiDes[6] = ((F << 19) + (F >> 13));
 		poiDes[7] = G;
 	}
-
 	//将64轮子函数的输出与最初输入异或作为结果
 	for (int i=0;i<8;i++)
 	{
-		result[i] = linkVariable[i] ^ poiDes[i];
+		result[i] = linkVar[i] ^ poiDes[i];
 	}
-
 	delete [8]poiSrc;
 	delete [8]poiDes;
 	delete wordList;
@@ -343,3 +347,9 @@ uint32* compress(uint32 *wordList,uint32 *linkVariable)
 }
 
 
+int endShift(uint32*m32,int l32,uint8*m8,int l8)
+{
+
+
+	return 1;
+}
--- Project 4 optimize SM3 implementation (software)/SM3.h
+++ Project 4 optimize SM3 implementation (software)/SM3.h
@@ -8,8 +8,9 @@ typedef unsigned long long uint64;
 using namespace std;
 
 void testSM3HashFunction();
+
 uint32 loopLeftShift(uint32 x, int dis);
-uint32** padding(uint8* plainText, long plainLength, int blockSize);
+uint32** padding(uint8* plainText, long long plainLength, long long blockSize);
 uint32*  messageExtend(uint32 mes[16]);
-uint32*  compress(uint32* wordList, uint32 *linkVariable);
-int SM3Hash(uint8* plainText, long plainLength,uint8 hashValue[32]);
+uint32*  compress(uint32 mes[16], uint32 linkVariable[8]);
+int SM3Hash(uint8* plainText, long long plainLength,uint8 hashValue[32]);
