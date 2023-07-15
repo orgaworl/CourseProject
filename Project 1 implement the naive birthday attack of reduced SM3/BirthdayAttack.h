3acc3115a3a01d8bad91da2bdfebc2332d01718d
orgaworl <orgaworl@outlook.com>
2023-07-15 16:50:09 +0800

    remove: remove all projects

--- Project 1 implement the naive birthday attack of reduced SM3/BirthdayAttack.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include"SM3.h"
-
-uint64 reducedSM3(uint8* plainText, long plainLength, int hashValueLength);
-int birthdayAttack(uint64 hashValue,int hashValueLength);
-void testBirthAttackOfSM3();
